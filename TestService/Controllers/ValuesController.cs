2f9e88bacbb9872b1be46832a0c0f2698cd8a486
foodtech24 <foodtech24@outlook.com>
2019-11-28 10:27:47 +0700

    Первая часть

--- TestService/Controllers/ValuesController.cs
+++ TestService/Controllers/ValuesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TestService.Models;
+using TestService.Actions;
+using Newtonsoft.Json;
 
 namespace TestService.Controllers
 {
@@ -26,8 +29,35 @@ namespace TestService.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public JsonResult Post([FromBody] Request value)
         {
+
+            Random rand = new Random();
+            var x = rand.Next(10000000, 99999999);
+            string session = x.ToString();
+
+            Program.Logger.Debug(" > "+session +" > "+JsonConvert.SerializeObject(value));
+
+
+            Result res = new Result();
+            switch (value.RequestIdent)
+            {
+                case "PositionAdd":
+                    ActPosition actPosition = new ActPosition();
+                    res = actPosition.Add(value);
+                    break;
+                case "PositionsGet":
+                    ActPosition actPositionGetAll = new ActPosition();
+                    res = actPositionGetAll.GetAll(value);
+                    break;
+                default:
+                    res.Code = 105;
+                    res.Info = "Не найден метод";
+                    break;
+            }
+
+            Program.Logger.Debug(" < " + session + " < " + JsonConvert.SerializeObject(res));
+            return new JsonResult(res);
         }
 
         // PUT api/values/5
